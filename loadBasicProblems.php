5a82143d67007787d037b43f541a7f8e2ae1bf77
ilbay <tugay.ilbay@gmail.com>
2013-12-22 03:35:41 +0200

    New elements are added to loadBasicProblems

--- loadBasicProblems.php
+++ loadBasicProblems.php
@@ -5,7 +5,7 @@
     $db = new SQLite3('istanbulweb.db');
     //$db->exec("PRAGMA encoding='UTF-8'");
     //TODO: Status filter is needed.
-    $results = $db->query("SELECT id,reportdate,latitude,longtitude,photo,description FROM PROBLEMS");
+    $results = $db->query("SELECT id,reportdate,latitude,longtitude,photo,description,category FROM PROBLEMS");
 
     $motherArray = array();
     $problems = array();
@@ -13,15 +13,18 @@
     while( $arr = $results->fetchArray(SQLITE3_ASSOC) )
     {
         $p = array();
-  
-        $p["problemid"] = $arr["id"];
+
         $problemid = $arr["id"];
+        $categoryid = $arr["category"];
+        
+        $p["problemid"] = $arr["id"];
         $p["latitude"] = $arr["latitude"];
         $p["longitude"] = $arr["longtitude"];
         $p["photo"] = $arr["photo"];
         $p["description"] = $arr["description"];
         $p["reportdate"] = $arr["reportdate"];
         $p["voteCount"] = $db->querySingle("SELECT count(id) FROM VOTES WHERE problem='$problemid'");
+        $p["category"] = $db->querySingle("SELECT categoryName FROM CATEGORY WHERE id='$categoryid'");
         
         array_push($problems, $p);
     }
